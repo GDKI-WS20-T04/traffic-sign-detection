5fc9b51a8b060f52b8649e69dfeb26826207ef51
Max Stephan <max.stephan@mni.thm.de>
2021-02-10 09:06:23 +0100

    [app] install @tensorflow/tfjs-react-native

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { Component, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-react-native";
 
 export default function App() {
+  const [isTfReady, setIsTfReady] = useState(false);
+
+  useEffect(() => {
+    const initTf = async () => {
+      // Wait for tf to be ready.
+      await tf.ready();
+      // Signal to the app that tensorflow.js can now be used.
+      setIsTfReady(true);
+    };
+
+    initTf();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {isTfReady && <Text>Tf Ready</Text>}
     </View>
   );
 }
