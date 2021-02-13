f1fc043777a5006caa255ff028d3dcbfbd865f46
Max Stephan <max.stephan@mni.thm.de>
2021-02-13 13:50:48 +0100

    [app] request permissions

--- app/src/components/Home/index.tsx
+++ app/src/components/Home/index.tsx
@@ -1,8 +1,26 @@
+import { usePermissions } from "expo-permissions";
 import React from "react";
 import { View } from "react-native";
-import { Button } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
+import * as Permissions from "expo-permissions";
 
 export const Home = () => {
+  const [permission, askForPermission] = usePermissions(
+    [Permissions.CAMERA, Permissions.LOCATION],
+    {
+      ask: true,
+    }
+  );
+
+  if (!permission || permission.status !== "granted") {
+    return (
+      <View>
+        <Text>Permission is not granted</Text>
+        <Button onPress={askForPermission}>Grant permission</Button>
+      </View>
+    );
+  }
+
   return (
     <View>
       <Button
