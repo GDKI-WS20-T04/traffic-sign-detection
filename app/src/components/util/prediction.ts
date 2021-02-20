4fdb89a0b80c08140cd9f6cd089e021136ea3162
Max Stephan <max.stephan@mni.thm.de>
2021-02-20 11:42:08 +0100

    [app] added prediction utils

---
+++ app/src/components/util/prediction.ts
@@ -0,0 +1,50 @@
+import { getLabes, PredictionResult } from "./label";
+import * as tf from "@tensorflow/tfjs";
+import { decodeJpeg, fetch } from "@tensorflow/tfjs-react-native";
+import { Image } from "react-native";
+
+export const predict = async (
+  model: tf.GraphModel | undefined,
+  nextImageTensor: any
+): Promise<PredictionResult[]> => {
+  if (!model) return [];
+
+  console.time("t");
+  const res = ((await model?.executeAsync(
+    nextImageTensor?.expandDims(0)
+  )) as any) as tf.Tensor[];
+  console.timeEnd("t");
+
+  const classes = res[1].dataSync() as Float32Array;
+  const scores = res[5].dataSync() as Float32Array;
+
+  return getLabes(classes, scores);
+};
+
+export const predictVideoImage = async (
+  model: tf.GraphModel | undefined,
+  nextImageTensor: any
+) => {
+  const res = await predict(model, nextImageTensor);
+
+  console.log("Result: ", res);
+};
+
+export const predictImage = async (model?: tf.GraphModel) => {
+  if (!model) return;
+
+  console.log("start");
+  // Load an image as a Uint8Array
+  const imageUri = Image.resolveAssetSource(
+    require("../../assets/v5-detector_3/test_image.jpg")
+  ).uri;
+  const response = await fetch(imageUri, {}, { isBinary: true });
+  const imageDataArrayBuffer = await response.arrayBuffer();
+  const imageData = new Uint8Array(imageDataArrayBuffer);
+
+  // Decode image data to a tensor
+  const imageTensor = decodeJpeg(imageData);
+
+  const res = await predict(model, imageTensor);
+  console.log("finished: ", res);
+};
