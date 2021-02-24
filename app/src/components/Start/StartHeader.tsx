a98cc119a65e82bac6ab25973f4b2fa068d3b713
Max Stephan <max.stephan@mni.thm.de>
2021-02-24 15:00:59 +0100

    [app][StartHeader] clean up
    Former-commit-id: c9fe29e64270824d7b64fccd9525539c5eb8cee5

--- app/src/components/Start/StartHeader.tsx
+++ app/src/components/Start/StartHeader.tsx
@@ -1,7 +1,7 @@
 import { StackHeaderTitleProps } from "@react-navigation/stack";
 import React from "react";
-import { Image, View, Text, StyleSheet } from "react-native";
-import { Button, Colors, IconButton } from "react-native-paper";
+import { View, Text, StyleSheet } from "react-native";
+import { Colors, IconButton } from "react-native-paper";
 
 export const StartHeader: React.FC<StackHeaderTitleProps> = () => {
   return (
@@ -9,7 +9,7 @@ export const StartHeader: React.FC<StackHeaderTitleProps> = () => {
       <View style={style.textContainer}>
         <Text style={style.text}>Sign Detector</Text>
       </View>
-      <View style={style.indicator}>
+      <View>
         <IconButton icon="camera" color={Colors.red500} size={20} />
       </View>
     </View>
@@ -24,14 +24,10 @@ const style = StyleSheet.create({
   textContainer: {
     textAlign: "center",
     textAlignVertical: "center",
-    // backgroundColor: "#585858",
     flex: 1,
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
-  },
-  indicator: {
-    // backgroundColor: "#32EAFA",
   },
 });
