27df5d25bdabf7b5da2004652f87f544f7f22d18
Max Stephan <max.stephan@mni.thm.de>
2021-02-28 22:35:49 +0100

    [app][prediction] added timeout
    Former-commit-id: 59c68486954ca8ccc83fd01e8336a9731c86417c

--- app/src/api/prediction.ts
+++ app/src/api/prediction.ts
@@ -1,10 +1,15 @@
+import { fetchTimeOut } from "../components/util/fetchUtils";
 import { ImageResult } from "../components/util/image";
 
 export function postImage(image: string): Promise<ImageResult> {
   const formData = new FormData();
   formData.append("base64_image", image);
-  return fetch(`http://localhost/image`, {
-    method: "POST",
-    body: formData,
-  }).then((res) => res.json().catch((err) => {})) as Promise<ImageResult>;
+  return fetchTimeOut(
+    `http://localhost/image`,
+    {
+      method: "POST",
+      body: formData,
+    },
+    5000
+  ).then((res) => res.json().catch((err) => {})) as Promise<ImageResult>;
 }
