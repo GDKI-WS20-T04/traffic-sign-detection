d23bb1ff33efb90807ff06c070ed76a2d24eb321
Jannik-PC <jannik.lapp@mni.thm.de>
2021-03-04 11:44:29 +0100

    removed object_detection package (its only need when server saves the labeld images)
    Former-commit-id: cf5537c3e03f46a51b8d1ff11df51d6f37366cab

--- server/api/api.py
+++ server/api/api.py
@@ -1,9 +1,5 @@
 import io
-import json
 import time
-import matplotlib
-import matplotlib.pyplot as plt
-import cv2
 import flask
 import logging
 
@@ -12,8 +8,6 @@ from PIL import Image
 from flask import (Blueprint, request)
 import tensorflow as tf
 import base64
-from object_detection.utils import visualization_utils as viz_utils
-from object_detection.utils import label_map_util
 
 tf.get_logger().setLevel('ERROR')
 
@@ -30,16 +24,12 @@ PATH_TO_LABELS = 'E:/gdki-ws-20-21-projekt/ki/sign-identification/annotations/la
 
 detect_fn = tf.saved_model.load(PATH_TO_CKPT)
 
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
-                                                                    use_display_name=True)
 
 app = flask.Flask(__name__, static_url_path="/")
-i = 250
 
 
 @app.route("/image", methods=["POST"])
 def test_image():
-    global i
     start_time = time.time()
     try:
         if "base64_image" in request.values:
@@ -74,29 +64,6 @@ def test_image():
             "detection_scores": list(map(float, detections["detection_scores"])),
             "detection_boxes": boxes
         }
-
-        """
-        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
-        if detections['detection_scores'][0] >= 0.7:
-            image_np_with_detections = npimg.copy()
-
-            matplotlib.rcParams.update({'font.size': 22})
-            viz_utils.visualize_boxes_and_labels_on_image_array(
-                image_np_with_detections,
-                detections['detection_boxes'],
-                detections['detection_classes'],
-                detections['detection_scores'],
-                category_index,
-                use_normalized_coordinates=True,
-                max_boxes_to_draw=200,
-                min_score_thresh=.70,
-                agnostic_mode=False,
-            )
-            plt.figure()
-            plt.imshow(image_np_with_detections)
-            plt.savefig("new_img" + str(i) + ".jpg", transparent=True)
-            i = i + 1
-        """
 
         end(start_time)
 
